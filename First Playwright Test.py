f6d25a88e5928cf28587a07a7e2c6e587bcfb695
Thinh Ngo <nlhthinh95@gmail.com>
2025-08-05 23:04:28 +0700

    add file First Playwright Test.py

---
+++ First Playwright Test.py
@@ -0,0 +1,9 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False, slow_mo=1000)
+    page = browser.new_page()
+    page.goto("https://playwright.dev")
+    print(page.title())
+    browser.close()
+
--- test_abc.py
+++
@@ -1,4 +0,0 @@
-from playwright.sync_api import Page
-
-def test_navigate(page : Page):
-    page.goto("https://google.com")
