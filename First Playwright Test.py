01f45bfb4d8498e23a6c909350b8052468b59b2f
Thinh Ngo <nlhthinh95@gmail.com>
2025-08-07 00:17:09 +0700

    add file Trace_Viewer.py

--- First Playwright Test.py
+++ First Playwright Test.py
@@ -2,12 +2,12 @@ import asyncio
 from playwright.async_api import async_playwright
 from playwright.sync_api import sync_playwright
 
-# with sync_playwright() as p:
-#     browser = p.chromium.launch(headless=False, slow_mo=1000)
-#     page = browser.new_page()
-#     page.goto("https://playwright.dev")
-#     print(page.title())
-#     browser.close()
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False, slow_mo=1000)
+    page = browser.new_page()
+    page.goto("https://playwright.dev")
+    print(page.title())
+    browser.close()
 
 async def main():
     async with async_playwright() as p:
